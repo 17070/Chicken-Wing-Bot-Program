0f51bf9f303ccdd356001ca74db6e58e4e1d2466
Dante Lee Choi <17070@my.sanctamaria.school.nz>
2022-04-12 07:44:30 +1200

    qwa

--- Chicken Wing Ordering Menu v1.py
+++ Chicken Wing Ordering Menu v1.py
@@ -5,3 +5,6 @@ menu_items = ['Singular Chicken Wing','100 pack of Chicken Wings',
               'Turkey Wings','Chicken Wing Pizza', 'Chicken Grease Soda',
               'Chicken Wing Ice Cream', 'Turkey Joes Special Monkey Meat Meal',
               'Chicken Wing Fries', 'ChickenCakes']
+
+menu_cost = ['0.50', '45.00', '8,00(burger), 12,00(combo)', '12.00', '10.00', '5,00', '10,00',
+             '14.00', '3.00(s), 4.00(m), 5,00(L)', ]
